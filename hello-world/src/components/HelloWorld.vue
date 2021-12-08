a7eba6827ac51a4588cc959bd8843b4012e504e8
wuguang <wuguang_123@163.com>
2021-12-09 06:53:45 +0800

    update

--- hello-world/src/components/HelloWorld.vue
+++ hello-world/src/components/HelloWorld.vue
@@ -9,6 +9,7 @@
 
 
 <script>
+/*
 import {defineComponent,reactive,ref,toRefs} from "vue";
 export default defineComponent({
 	setup(){
@@ -26,6 +27,41 @@ export default defineComponent({
 		}
 	}
 })
+*/
+import { defineComponent, onBeforeMount, onMounted, onBeforeUpdate,onUpdated,
+onBeforeUnmount, onUnmounted, onErrorCaptured, onRenderTracked,
+onRenderTriggered } from "vue"; 
+
+export default defineComponent({ 
+	/*
+	//beforeCreate和created是vue2的 
+	beforeCreate() {
+		console.log("------beforeCreate-----"); 
+	}, 
+	created() {
+		console.log("------created-----"); 
+	}, 
+	
+
+	setup() { console.log("------setup-----");
+
+		// vue3.x生命周期写在setup中 onBeforeMount(() => {
+		console.log("------onBeforeMount-----"); 
+	}),
+
+	onMounted(() => {
+		console.log("------onMounted-----"); 
+	}),
+	
+	// 调试哪些数据发生了变化
+	onRenderTriggered((event) =>{ 
+		console.log("------onRenderTriggered-----",event);
+	})
+	*/
+
+});
+
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
---
+++ hello-world/src/components/Two.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="homePage">
+    <p>第 {{ year }} 年</p>
+    <p>姓名： {{ nickname }}</p>
+    <p>年龄： {{ age }}</p>
+  </div>
+</template>
+
+
+
+<script>
+
+import {defineComponent,reactive,toRefs,watch} from "vue";
+export default defineComponent({
+	setup(){
+	
+		const state = reactive({ nickname: "xiaofan", age: 26})
+
+		setTimeout(() => {
+			state.age++;
+		}, 1000);
+
+		watch(
+			()=>state.age,
+			(curAge,preAge)=>{
+				console.log(`newValue = ${curAge}----oldValue = ${preAge}`);
+			}
+		)
+
+		return {
+			...toRefs(state)
+		}
+	}
+})
+
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
